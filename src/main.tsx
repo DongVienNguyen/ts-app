11b64989c682f8500270815bd9d00c8179b6a3ed
[dyad] <git@dyad.sh>
2025-07-08 16:08:05 +0700

    [dyad] wrote 1 file(s)

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import { SecureAuthProvider } from './hooks/useSecureAuth'; // Import SecureAuthProvider
-import React from 'react'; // Import React for StrictMode
+import { SecureAuthProvider } from './hooks/useSecureAuth.tsx'
+import React from 'react'
 
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <SecureAuthProvider> {/* Wrap App with SecureAuthProvider */}
+    <SecureAuthProvider>
       <App />
     </SecureAuthProvider>
   </React.StrictMode>
