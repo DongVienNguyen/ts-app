b3cbc0d47af03568c654874e7a5fe9a4c0748f21
[dyad] <ngviendong@gmail.com>
2025-07-11 15:51:49 +0700

    [dyad] Fixed Vite config error by changing vite-plugin-compression import from named import {compression} to default import viteCompression and updated plugin usage accordingly - wrote 1 file(s) + extra files edited outside of Dyad

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 import { visualizer } from 'rollup-plugin-visualizer'
-import { compression } from 'vite-plugin-compression'
+import viteCompression from 'vite-plugin-compression'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -13,13 +13,13 @@ export default defineConfig({
     }),
     
     // Gzip compression
-    compression({
+    viteCompression({
       algorithm: 'gzip',
       ext: '.gz'
     }),
     
     // Brotli compression (better than gzip)
-    compression({
+    viteCompression({
       algorithm: 'brotliCompress',
       ext: '.br'
     }),
