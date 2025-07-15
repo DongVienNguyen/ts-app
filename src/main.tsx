f8de48f5fda99114affdf31e720cca12b8552048
[dyad] <ngviendong@gmail.com>
2025-07-15 10:09:06 +0700

    [dyad] Fixing missing QueryClientProvider - wrote 1 file(s)

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 console.log('🚀 Asset Management System starting...');
+
+// Create a client
+const queryClient = new QueryClient()
 
 // Set initial light theme on document
 document.documentElement.classList.remove('dark');
@@ -11,6 +15,8 @@ document.documentElement.style.colorScheme = 'light';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </React.StrictMode>,
 )
