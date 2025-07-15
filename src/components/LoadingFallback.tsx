aa796c937809cdf52cc50dad7e9c0bd59ae81f45
[dyad] <ngviendong@gmail.com>
2025-07-15 11:16:15 +0700

    [dyad] Fixing unused import error - wrote 1 file(s)

--- src/components/LoadingFallback.tsx
+++ src/components/LoadingFallback.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 const LoadingFallback = () => (
   <div className="flex items-center justify-center h-screen w-full">
     <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-b-4 border-green-600"></div>
